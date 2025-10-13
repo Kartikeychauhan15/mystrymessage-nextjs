9243a548a1943dddbe431d42a93760b5a7afc762
Kartikey chauhan <itskartikeyy15@gmail.com>
2025-10-13 22:14:33 +0530

    react hook form, shadcn and debouncing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,10 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import AuthProvider from '../context/AuthProvidex';
-import { Toaster } from '@/components/ui/toaster';
+import { Toaster } from "@/components/ui/sonner"
 const inter = Inter({ subsets: ['latin'] });
+
+
 
 export const metadata: Metadata = {
   title: 'True Feedback',
